2258419091cee99a2780dc665e3c3d97db862d3e
Your Name <you@example.com>
2022-08-10 03:22:07 +0530

    Added: saying hello by the path variable

--- src/main/java/com/bridglabz/firstspringapp/controller/HelloRestController.java
+++ src/main/java/com/bridglabz/firstspringapp/controller/HelloRestController.java
@@ -1,14 +1,13 @@
 package com.bridglabz.firstspringapp.controller;
 
-import com.bridglabz.firstspringapp.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
-    @PutMapping("/put/{fName}")
-    public String sayHello(@PathVariable String fName,@RequestParam(value = "lName") String lName){
-        return "Hello  "+fName+" "+lName+" !";
+	 @GetMapping(value = { "","/","/home"})
+	    public String sayHello(){
+	        return "Hello From BridgeLabz";
     }
 
 
